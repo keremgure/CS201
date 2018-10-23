c112b7a2332950d474753eda931fe27ed269ab71
Kerem Gure <kerem.gure@ozu.edu.tr>
2018-10-23 17:46:55 +0300

    Fixed the issue where the popped items from the stack were not getting deleted after being stored in the vector, Also added some comments.

--- HW2/Stack.cpp
+++ HW2/Stack.cpp
@@ -2,7 +2,7 @@
 
 Stack::Stack() {
     this->head = nullptr;
-    this->vec = new vector<StackItem*>;
+    this->vec = new vector<StackItem*>();
 }
 
 Stack::~Stack() {
@@ -10,6 +10,10 @@ Stack::~Stack() {
         delete this->head;
         head = nullptr;
     }
+    //Delete the Popped Items.
+    for(StackItem* item : *this->vec)
+        delete item;
+    //Wipe the vector.
     this->vec->erase(this->vec->begin(),this->vec->end());
     delete this->vec;
     this->vec = nullptr;
@@ -25,6 +29,7 @@ StackItem* Stack::pop() {
     StackItem *temp = this->head;
     this->head = this->head->next;
     temp->next = nullptr;
+    //Keep track of the popped item so that we can delete it when we no longer need it.
     this->vec->push_back(temp);
 
     return temp;
