c0a394a602fa46f35ece9816c9737699e91998b0
Kerem Gure <kerem.gure@ozu.edu.tr>
2018-12-10 21:32:19 +0300

    Fixed a typo in the code.

--- HW4/AlgorithmSortQuick.cpp
+++ HW4/AlgorithmSortQuick.cpp
@@ -10,49 +10,48 @@ AlgorithmSortQuick::~AlgorithmSortQuick() {
 }
 
 int AlgorithmSortQuick::select() {
-    for(int m = 0; m<N;m++)
-        cin >> arr[m];
-    return quickselect(arr,0,N-1,k);
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+    return quickselect(arr, 0, N - 1, k);
 }
 
-int AlgorithmSortQuick::quickselect(int * arr,int left, int right, int k) {
-    if(left + 10 <= right){
-        int pivot = median(arr,left,right);
+int AlgorithmSortQuick::quickselect(int *arr, int left, int right, int k) {
+    if (left + 10 <= right) {
+        int pivot = median(arr, left, right);
         int i = left;
         int j = right - 1;
 
-        while(true){
-            for(;arr[++i] < pivot;);
-            for(;pivot < arr[--j];);
-            if(i<j)
-                swap(arr[i],arr[j]);
+        while (true) {
+            for (; arr[++i] < pivot;);
+            for (; pivot < arr[--j];);
+            if (i < j)
+                swap(arr[i], arr[j]);
             else break;
         }
-        swap(arr[i],arr[right-1]); //restore the pivot.
-        return (k <= i-1) ? quickselect(arr,left,i-1,k) : ((k==1) ? pivot : quickselect(arr,i+1,right ,k));
-    }else
+        swap(arr[i], arr[right - 1]); //restore the pivot.
+        return (k <= i - 1) ? quickselect(arr, left, i - 1, k) : ((k == i) ? pivot : quickselect(arr, i + 1, right, k));
+    } else
         insertionSort(arr, left, right);
     return arr[k];
 
 }
 
-int AlgorithmSortQuick::median(int * arr, int left, int right) {
+int AlgorithmSortQuick::median(int *arr, int left, int right) {
     int center = (left + right) / 2;
-    if(arr[center] < arr[left])
-        swap(arr[left],arr[center]);
-    if(arr[right] < arr[left])
-        swap(arr[right],arr[left]);
-    if(arr[right] < arr[center])
-        swap(arr[center],arr[right]);
+    if (arr[center] < arr[left])
+        swap(arr[left], arr[center]);
+    if (arr[right] < arr[left])
+        swap(arr[right], arr[left]);
+    if (arr[right] < arr[center])
+        swap(arr[center], arr[right]);
 
-    swap(arr[center],arr[right-1]);
-    return arr[right-1];
+    swap(arr[center], arr[right - 1]);
+    return arr[right - 1];
 }
 
 void AlgorithmSortQuick::insertionSort(int *arr, int left, int right) {
-    int j;
-    for (int i = left+1; i < right+1; i++){
-        j = i - 1;
+    for (int i = left + 1; i < right + 1; i++) {
+        int j = i - 1;
         int key = arr[i];
 
         while (j >= left && arr[j] > key) {
